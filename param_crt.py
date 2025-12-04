3c6d35585dde3ff8715e4779859253f0fa423cef
ViniBilck <bilck.vinicius1998@gmail.com>
2025-12-04 14:58:01 -0300

    configs

--- param_crt.py
+++ param_crt.py
@@ -1,5 +1,4 @@
 import os
-from iccr import Collision
 import pandas as pd
 
 def create_params(file_name, param_template, output_path, ic_path):
@@ -34,15 +33,45 @@ def create_ics(data_csv_file, galaxy_file1, galaxy_file2):
         print("Directory " , dirName ,  " already exists")
 
     data_ic = data_csv_file
-    _collision = Collision(galaxy_file1, galaxy_file2)
-    _collision.create_file()
-    _collision.initial_condition_file(pericenter=0, orbit=data_ic)
+    fnameIn = "config.ini" #criar configs
+    ret=subprocess.call("collision " + fnameIn, shell=True, stdout=subprocess.DEVNULL)
+    ret=subprocess.call("mv orbits_pot_hbd_din_fric.dat " + fnameOrbit, shell=True, stdout=subprocess.DEVNULL)
 
-def open_coord_csv(csv_file):
-    pd.open_csv(csv_file)
-    #pegar dados do csv
-    # cubo [[galaxy1_coords, galaxy2_coords, galaxy1_vels, galaxy2_vels],[..],...]
-    return  #retorna array com todas [coord, vel]
+def create_config():
+    dirName = "config_temp"
+    if not os.path.exists(dirName):
+        os.mkdir(dirName)
+        print("Directory " , dirName ,  " Created ")
+    else:
+        print("Directory " , dirName ,  " already exists")
+
+    j = 0
+    try:
+        for i in sorted(os.listdir('orbits_temp/')):
+            fname = i
+            if fname != '.ipynb_checkpoints':   
+                orb = pd.read_csv(f'orbits_temp/{fname}', sep="\t")
+                data_initial = orb.loc[0]
+                config_template = "config_template.ini"
+                file_name = f"config_{j}.ini"
+                save_configs = os.path.join(dirName, file_name)
+                data = [data_initial.x_kepl, data_initial.y_kepl, data_initial.z_kepl,
+                        data_initial.Vx_kepl, data_initial.Vy_kepl, data_initial.Vz_kepl] #dados do queorbita
+                replacements = {"__g1cx__":data[0] , "__g1cy__":data[1],"__g1cz__":data[2],
+                                "__g1vx__":data[3], "__g1vy__":data[4],"__g1vz__":data[5]}
+                
+                with open(config_template, 'r', encoding='utf-8') as param:
+                    content = param.read()
+                with open(save_configs, 'w', encoding='utf-8') as file:
+                    for template_data_, data_ in replacements.items():
+                        content = content.replace(template_data_, str(data_))
+                    file.write(content)
+                    file.close()
+                    param.close()
+                j+=1
+    except Exception as e:
+        print(f"Drop orbits_temp directory in the directory: {e}")
+    return 0
 
 def make_chain(csv_file, galaxy_file1, galaxy_file2):
     for data in open_coord_csv(csv_file):
@@ -50,4 +79,4 @@ def make_chain(csv_file, galaxy_file1, galaxy_file2):
         create_params(...)
 
     return None
-create_params("teste", "param_template.txt", "../teste", "../ic_teste")
+create_config()
